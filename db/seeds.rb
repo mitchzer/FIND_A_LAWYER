2e08c094dc59c1cb0f29a287c80c08f0e17526bd
Michel Wlodarczyk <michel.wlodarczyk@edu.escpeurope.eu>
2018-05-29 15:06:53 +0100

    models

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,14 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
 
 
+User.create!(first_name: "Abel", email: "Abel@Abel.Abel", password: "metzplusgrand")
+User.create!(first_name: "Abdel", email: "Abdel@Abdel.Abdel", password: "metzplusgrand2")
+User.create!(first_name: "Alphonse", email: "Alphonse@Alphonse.Alphonse", password: "metzplusgrand3")
+User.create!(first_name: "Alain", email: "Alain@Alain.Alain", password: "metzplusgrand4")
+
+Lawyer.create!(user_id: "1", specialties: ["crime"])
+Lawyer.create!(user_id: "2", specialties: ["family"])
+Lawyer.create!(user_id: "3", specialties: ["business"])
