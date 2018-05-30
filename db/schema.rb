13eee036f3f08d986eafea502a1f70a24ace29d7
Anthony McShane <a89th@hotmail.com>
2018-05-30 16:17:07 +0100

    check databaes2

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2018_05_30_115633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
+    t.boolean "lawyer"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
