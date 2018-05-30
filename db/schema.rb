bc41ddcbc478237ef67449d797196497b475b991
AbelBoot <abellocati@hotmail.com>
2018-05-30 14:57:28 +0100

    updated forms to create lawyer and create user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_05_29_153725) do
-=======
 ActiveRecord::Schema.define(version: 2018_05_30_115633) do
->>>>>>> adb87e209b777ca98e5ac25069a219ad3843328c
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,11 +51,7 @@ ActiveRecord::Schema.define(version: 2018_05_30_115633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
-<<<<<<< HEAD
-    t.string "last_name"
     t.boolean "lawyer"
-=======
->>>>>>> adb87e209b777ca98e5ac25069a219ad3843328c
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
