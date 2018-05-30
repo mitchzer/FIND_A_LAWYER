f8a92e37de55e4a99ae0d1d195659ed61c934694
AbelBoot <abellocati@hotmail.com>
2018-05-30 18:49:35 +0100

    add geocoder method and validation in lawyer model

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -1,6 +1,7 @@
 class Lawyer < ApplicationRecord
   has_many :appointments
   belongs_to :user
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :professional_email, presence: true
 end
