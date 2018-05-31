e076936a6ade2cc1cfd021f3278c10d08936c988
Anthony McShane <a89th@hotmail.com>
2018-05-31 16:27:39 +0100

    basic search function implemented for name, location or specialty

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -2,6 +2,10 @@ class Lawyer < ApplicationRecord
   include PgSearch
   pg_search_scope :search_by_specialties_and_address,
     against: [ :specialties, :address ],
+    associated_against: {
+      specialty: [ :name ],
+      user: [ :first_name ]
+    },
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
